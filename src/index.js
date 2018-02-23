be7138db8f1a0fd095bfb21c27fc68e57a0ee0bf
Maksim Kazachenka <sozhskifront@gmail.com>
2018-02-24 00:26:11 +0300

    test

--- src/index.js
+++ src/index.js
@@ -1,30 +1,57 @@
 class Sorter {
+
   constructor() {
-    // your implementation
+    this.list = [];
+    this.comporator = null;
   }
 
   add(element) {
-    // your implementation
+    this.list.push(element);
   }
 
   at(index) {
-    // your implementation
+    return this.list[index];
   }
 
   get length() {
-    // your implementation
+    return this.list.length;
   }
 
   toArray() {
-    // your implementation
+    return this.list;
   }
 
   sort(indices) {
-    // your implementation
+    indices.sort();
+    if(indices.length < 2){
+      return this.list;
+    }
+    for (var i = 0; i < indices.length - 1; i++) {
+      for (var j = i+1; j < indices.length; j++) {
+        var index1 = indices[i];
+        var index2 = indices[j];
+        if(this.comporator == null){
+          if(this.list[index1]>this.list[index2]){
+            var numb = this.list[index1];
+            this.list[index1] = this.list[index2];
+            this.list[index2] = numb;
+          }
+        }
+        else{
+          // console.log(this.comporator(this.list[index1],this.list[index2]));
+          if(this.comporator(this.list[index1],this.list[index2]) > 0){
+            var numb = this.list[index1];
+            this.list[index1] = this.list[index2];
+            this.list[index2] = numb;
+          } 
+        }
+      }
+    }
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    this.comporator = compareFunction;
+    // console.log(this.comporator);
   }
 }
 
